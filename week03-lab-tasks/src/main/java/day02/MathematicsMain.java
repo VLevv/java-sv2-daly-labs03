109a1bfeced082bd619afdea132bb34aa1d4d06d
VLevv <vadaslevi@gmail.com>
2021-10-26 15:42:50 +0200

    Keddi konzultáció feladat

---
+++ week03-lab-tasks/src/main/java/day02/MathematicsMain.java
@@ -0,0 +1,16 @@
+package day02;
+
+public class MathematicsMain {
+    public static void main(String[] args) {
+        Mathematics math =new Mathematics();
+        System.out.println(math.isPrime(2));
+        System.out.println(math.isPrime(3));
+        System.out.println(math.isPrime(4));
+        System.out.println(math.isPrime(5));
+        System.out.println(math.isPrime(6));
+        System.out.println(math.isPrime(7));
+        System.out.println(math.isPrime(8));
+        System.out.println(math.isPrime(9));
+        System.out.println(math.isPrime(1001));
+    }
+}
